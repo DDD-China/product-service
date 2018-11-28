ec907207c761bc5bee3695558d1cf3f2998c1224
Junfeng Dai <ryandjf@hotmail.com>
2018-11-28 22:41:01 +0800

    fix build

--- src/main/java/com/dmall/productservice/application/ProductFactory.java
+++ src/main/java/com/dmall/productservice/application/ProductFactory.java
@@ -1,10 +1,6 @@
 package com.dmall.productservice.application;
 
-<<<<<<< HEAD:src/main/java/com/dmall/productservice/application/ProductFactory.java
 import com.dmall.productservice.domain.Product;
-=======
-import com.dmall.productserivce.domain.Product;
->>>>>>> product add feature.:src/main/java/com/dmall/productserivce/application/ProductFactory.java
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -14,14 +10,14 @@ import java.math.BigDecimal;
 @Component
 public class ProductFactory {
 
-  protected static final ModelMapper mapper = new ModelMapper();
+    protected static final ModelMapper mapper = new ModelMapper();
 
-  public Product create(String name, String description, BigDecimal price, Boolean isOnSale) {
-    return Product.builder()
-        .name(name)
-        .description(description)
-        .price(price)
-        .isOnSale(isOnSale)
-        .build();
-  }
+    public Product create(String name, String description, BigDecimal price, Boolean isOnSale) {
+        return Product.builder()
+                .name(name)
+                .description(description)
+                .price(price)
+                .isOnSale(isOnSale)
+                .build();
+    }
 }
