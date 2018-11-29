30d73d7564759ddd5c26d84bd02e1cfa9ad43b04
Junfeng Dai <ryandjf@hotmail.com>
2018-11-29 20:48:25 +0800

    add test

---
+++ src/test/java/com/dmall/productservice/unittest/ProductServiceTest.java
@@ -0,0 +1,40 @@
+package com.dmall.productservice.unittest;
+
+import com.dmall.productservice.application.ProductService;
+import com.dmall.productservice.domain.Product;
+import com.dmall.productservice.infrastructure.repositories.ProductRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
+
+public class ProductServiceTest {
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Mock
+    private ProductRepository repository;
+
+    @Before
+    public void initMocks(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void shouldCallRepositoryToFindProductById() {
+        Long productId = 100L;
+        Product expected = new Product(productId, "iPhone", "newest iPhone", new BigDecimal(1000.00));
+
+        when(repository.findById(productId)).thenReturn(expected);
+
+        productService.getProductsById(productId);
+        verify(repository, atLeastOnce()).findById(productId);
+    }
+}
